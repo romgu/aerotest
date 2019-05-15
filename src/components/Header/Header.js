6ccbc45e20693c39cd7ac967f668e7acf5d0a572
romgu <gmarca20@gmail.com>
2019-05-15 12:00:16 -0300

    working on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,8 @@ import header from './header-x1.png';
 
 const Header = () => {
 	return (
-		<div className='header-Cover'>
-			<span>Electronics</span>
-			<img src={header} alt="Header" />
+		<div className='header-Cover inline-flex items-end'>
+			<span className="pl6 pb4 f2 fw6 near-white">Electronics</span>
 		</div>
 	)
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ import coin from './coin.svg';
 const Navigation = () => {
 	return (
 		<nav className=" flex items-center justify-between ph4 pv1">
-			<img style={{height: 'auto', width: '2%'}} src={logo} alt="Logo" />
+			<img style={{height: 'auto', width: '1.5rem'}} src={logo} alt="Logo" />
 			<div id='userInfo'>
 				<p className="di ph3">John Kite</p>
 				<div className="inline-flex items-center button">
