4ca253342f236773293efa515cebccc4496cf8b9
romgu <gmarca20@gmail.com>
2019-05-15 11:28:41 -0300

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './aerolab-logo.svg';
+import coin from './coin.svg';
+
+const Navigation = () => {
+	return (
+		<nav className=" flex items-center justify-between ph4 pv1">
+			<img src={logo} alt="Logo" />
+			<div id='userInfo'>
+				<p className="dib ph3">John Kite</p>
+				<div className="inline-flex items-center button">
+					<span className="ph1">6666</span>
+					<img src={coin} alt="Coin" style={{height: 'auto', width: '35%'}}/>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+export default Navigation;
